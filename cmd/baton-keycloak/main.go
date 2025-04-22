087230a88c569e340c020343fc3c3492c670caa4
Spiros <106523063+spiros-spiros@users.noreply.github.com>
2025-04-22 13:51:49 +0100

    ten

--- cmd/baton-keycloak/main.go
+++ cmd/baton-keycloak/main.go
@@ -5,44 +5,41 @@ package main
 // this is mainly so that when someone at Weaviate tries to access a cluster, C1 can add them to the right group on a JIT basis.
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"github.com/spiros-spiros/baton-keycloak/pkg/connector"
-	"github.com/spiros-spiros/baton-keycloak/pkg/keycloak"
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/conductorone/baton-sdk/pkg/logging"
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	ctx := context.Background()
-	// these need to be secrets spiros - don't forget to make the secrets
-	cfg := &config{
-		ServerURL:    os.Getenv("KEYCLOAK_SERVER_URL"),
-		Realm:        os.Getenv("KEYCLOAK_REALM"),
-		ClientID:     os.Getenv("KEYCLOAK_CLIENT_ID"),
-		ClientSecret: os.Getenv("KEYCLOAK_CLIENT_SECRET"),
+
+	cmd := &cobra.Command{
+		Use:   "baton-keycloak",
+		Short: "Baton Keycloak connector",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			logger := logging.FromContext(ctx)
+
+			connector.RegisterCmd(cmd)
+
+			err := cmd.Execute()
+			if err != nil {
+				logger.Error().Err(err).Msg("error running connector")
+				return err
+			}
+
+			return nil
+		},
 	}
 
-	if err := run(ctx, cfg); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	cmd.PersistentFlags().BoolP("debug", "d", false, "Enable debug logging")
+	cmd.PersistentFlags().StringP("output", "o", "", "Output file path")
+	cmd.PersistentFlags().StringP("format", "f", "json", "Output format (json, yaml)")
+
+	err := cmd.ExecuteContext(ctx)
+	if err != nil {
 		os.Exit(1)
 	}
 }
-
-type config struct {
-	ServerURL    string
-	Realm        string
-	ClientID     string
-	ClientSecret string
-}
-
-func run(ctx context.Context, cfg *config) error {
-	client := keycloak.NewClient(cfg.ServerURL, cfg.Realm, cfg.ClientID, cfg.ClientSecret)
-	if err := client.Connect(ctx); err != nil {
-		return err
-	}
-
-	c := connector.New(client)
-	return cli.Start(ctx, c)
-}
-
---
+++ pkg/connector/cli.go
@@ -0,0 +1,98 @@
+package connector
+
+import (
+	"context"
+
+	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/logging"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/spiros-spiros/baton-keycloak/pkg/keycloak"
+)
+
+var configRequiredFlags = []string{"keycloak-server-url", "keycloak-realm", "keycloak-client-id", "keycloak-client-secret"}
+
+type Config struct {
+	ServerURL    string `mapstructure:"keycloak-server-url"`
+	Realm        string `mapstructure:"keycloak-realm"`
+	ClientID     string `mapstructure:"keycloak-client-id"`
+	ClientSecret string `mapstructure:"keycloak-client-secret"`
+}
+
+func (c *Config) Validate() error {
+	for _, f := range configRequiredFlags {
+		if viper.GetString(f) == "" {
+			return cli.NewRequiredFlagError(f)
+		}
+	}
+	return nil
+}
+
+func (c *Config) Load() error {
+	err := viper.Unmarshal(c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func New(ctx context.Context, cfg *Config) (*Connector, error) {
+	client, err := keycloak.NewClient(ctx, cfg.ServerURL, cfg.Realm, cfg.ClientID, cfg.ClientSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	return New(client), nil
+}
+
+func RegisterCmd(parent *cobra.Command) {
+	config := &Config{}
+	cmd := &cobra.Command{
+		Use:   "keycloak",
+		Short: "Keycloak connector",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			logger := logging.FromContext(ctx)
+
+			err := config.Load()
+			if err != nil {
+				return err
+			}
+
+			err = config.Validate()
+			if err != nil {
+				return err
+			}
+
+			connector, err := New(ctx, config)
+			if err != nil {
+				return err
+			}
+
+			builder := connectorbuilder.NewConnectorBuilder(connector)
+			err = builder.Run(ctx)
+			if err != nil {
+				logger.Error().Err(err).Msg("error running connector")
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().String("keycloak-server-url", "", "The URL of the Keycloak server")
+	cmd.Flags().String("keycloak-realm", "", "The Keycloak realm to connect to")
+	cmd.Flags().String("keycloak-client-id", "", "The Keycloak client ID")
+	cmd.Flags().String("keycloak-client-secret", "", "The Keycloak client secret")
+
+	for _, f := range configRequiredFlags {
+		err := viper.BindPFlag(f, cmd.Flags().Lookup(f))
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	parent.AddCommand(cmd)
+}
+
